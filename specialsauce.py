7405db5db47f553d58190c73385a62a5e3b5cb71
fuutballer <david.miguel@singaporetech.edu.sg>
2022-03-21 04:01:46 +0000

    Second Commit

---
+++ specialsauce.py
@@ -0,0 +1,16 @@
+
+def nut():
+  tags = ["vanilla", "gyaru", "loli", "netorare", "ugly bastard", "cheating", "schoolgirl"]
+  for i in tags:
+    if i == "netorare":
+      print(f"skip this {i} doujin")
+      continue
+    if i == "cheating":
+      print(f"skip this {i} doujin")
+      continue
+    if i == "ugly bastard":
+      print(f"skip this {i} doujin")
+      continue
+    print(f"nut to this {i} doujin")
+
+nut()
